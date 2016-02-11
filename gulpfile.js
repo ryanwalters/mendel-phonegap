21d570ba2609423cc24209d58119918a3622e544
Ryan Walters <rwalters@eb.com>
2016-02-11 14:47:49 -0600

    added sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,20 @@
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 
 const paths = {
+    css: './www/sass/**/*.css',
+    dist: './www/dist',
     scripts: ['./www/app/**/_*.js', './www/app/**/*.js']
 };
+
+gulp.task('css', () => {
+    return gulp.src(paths.css)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.dist))
+});
 
 gulp.task('scripts', () => {
     return gulp.src(paths.scripts)
@@ -16,11 +25,12 @@ gulp.task('scripts', () => {
             presets: ['es2015']
         }))
         .pipe(uglify())
-        .pipe(gulp.dest('./www/dist'));
+        .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('watch', () => {
+    gulp.watch(paths.css, ['css']);
     gulp.watch(paths.scripts, ['scripts']);
 });
 
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['css', 'scripts', 'watch']);
